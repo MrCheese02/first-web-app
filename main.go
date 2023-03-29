cb6adb1e040f178e6564864b7bd03e57da768fed
simon <simon.tilzer@outlook.com>
2023-03-29 14:58:32 +0200

    Adding congigured server and mux. Added some addtional routes.

--- main.go
+++ main.go
@@ -3,13 +3,49 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"time"
 )
 
-func helloWorldPage(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+func handleFunction(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		fmt.Fprint(w, "Hello World")
+	case "/mrcheese":
+		fmt.Fprint(w, "Simon")
+	default:
+		fmt.Fprint(w, "ERROR!!!")
+	}
+
+	fmt.Printf("Handling function with %s request\n", r.Method)
+}
+
+func htmlVsPlain(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("htmlVsPlain")
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Hello World</h1>")
+}
+
+func timeout(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Timeout attempt")
+	time.Sleep(2 * time.Second)
+	fmt.Fprint(w, "Did *not* timeout")
 }
 
 func main() {
-	http.HandleFunc("/", helloWorldPage)
-	http.ListenAndServe("", nil)
+	http.HandleFunc("/", htmlVsPlain)
+	http.HandleFunc("/timeout", timeout)
+	// http.ListenAndServe("", nil)
+
+	server := http.Server{
+		Addr:         "",
+		Handler:      nil,
+		ReadTimeout:  1000,
+		WriteTimeout: 1000,
+	}
+
+	var myMux http.ServeMux
+	server.Handler = &myMux
+	myMux.HandleFunc("/", htmlVsPlain)
+
+	server.ListenAndServe()
 }
